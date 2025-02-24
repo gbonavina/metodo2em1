57f03e50bf084a40a3695724994e003f611feacb
gbonavina <gabriellbonavina@gmail.com>
2025-02-24 03:41:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+# Instala as dependências de sistema necessárias para o Playwright
+sudo apt-get update && sudo apt-get install -y \
+    libnss3 \
+    libnspr4 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libxkbcommon0 \
+    libasound2 \
+    libatspi2.0-0
+
+# Instala os navegadores do Playwright
 playwright install-deps
 playwright install chromium
